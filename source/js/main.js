d5dd24070de22e14533d19af9d0ef823c44817b6
tallergorilas <info@tallergorilas.com>
2018-03-21 23:15:13 +0100

    The path produced while draggin is simplified after being drawn

--- source/js/main.js
+++ source/js/main.js
@@ -4,23 +4,29 @@ window.onload = function() {
   // Create a simple drawing tool:
   var tool = new Tool();
   var path;
-  var path2;
 
-  // Define a mousedown and mousedrag handler
-  /*tool.onMouseDown = function(event) {
+  tool.onMouseDown = function(event) {
+    // If we produced a path before, deselect it:
+    if (path) {
+      path.selected = false;
+    }
+
+    // Create a new path and set its stroke color to black:
     path = new Path();
+    path.segments = [event.point];
     path.strokeColor = 'white';
+  }
+
+  // While the user drags the mouse, points are added to the path
+  // at the position of the mouse:
+  tool.onMouseDrag = function(event) {
     path.add(event.point);
   }
 
-  tool.onMouseDrag = function(event) {
-    path.add(event.point);
-  }*/
-
-  path = new Path();
-  path.strokeColor = 'white';
-  tool.onMouseMove = function(event) {
-    path.add(event.point);
+  // When the mouse is released, we simplify the path:
+  tool.onMouseUp = function(event) {
+    // When the mouse is released, simplify it:
+    path.simplify(10);
   }
 }
 

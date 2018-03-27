5dc02ac5f67b0860aa7ae81fdac8568ca16495b9
tallergorilas <info@tallergorilas.com>
2018-03-27 17:41:04 +0200

    The page appears with fadein

--- source/js/main.js
+++ source/js/main.js
@@ -4,6 +4,15 @@
 
 
 
+// Fade in
+
+window.addEventListener("load", function(event)
+{
+    console.log("Web page fully Loaded. HTML, Javascript, CSS, Images, Iframes and objects are fully loaded.");
+    document.body.classList.add("loaded");
+});
+
+
 // Call Accordion widgets
 
 new AccordionWidget(document.getElementsByClassName('accordion-interface')[0], null);
